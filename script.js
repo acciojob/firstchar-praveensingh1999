a5c8ba02eb1fd1bf1037a92ff42b8370d35b6ac6
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-05-22 21:21:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function firstChar(text) {
-	let str = text.toString();
-  // your code here
-	if(str.length===0){
-		return "";
-	}
-  return str[0];
+	console.log(typeof text);
+	// let str = text.toString();
+ //  // your code here
+	// if(str.length===0){
+	// 	return "";
+	// }
+ //  return str[0];
 	
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
 //const text = prompt("Enter text:");
-alert(firstChar(text));
+//alert(firstChar(text));
